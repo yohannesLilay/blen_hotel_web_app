a4bc374f3d9f70c8353c732ef2425d2a4c8d95de
Yohannes Lilay <yohannesLilay@gmail.com>
2023-10-02 09:13:25 +0300

    feat: Add company route to menu items configuration
    Added the company route to menu-items configuration in the "Configurations" group. This item has a corresponding URL and icon for navigation.

--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -1,11 +1,9 @@
 import dashboard from "./dashboard";
 import security from "./security";
 import configurations from "./configurations";
-import businessContacts from "./business-contacts";
-import transactions from "./transactions";
 
 const menuItems = {
-  items: [dashboard, security, configurations, businessContacts, transactions],
+  items: [dashboard, security, configurations],
 };
 
 export default menuItems;
