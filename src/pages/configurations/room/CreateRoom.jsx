526ba6c9442d38bc7403739e7a4e1c1a4661962d
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-11-24 12:09:15 +0300

    feat: Add CreateRoom component for room creation
    - Introduce CreateRoom component to facilitate the creation of new rooms.
    - Implement Yup for form validation, ensuring that required fields are filled and validated.
    - Utilize Formik for form state management, handling form submission and validation.
    - Utilize room-related API queries and mutations from roomApiSlice, including useCreateRoomMutation and useGetRoomTemplateQuery.
    - Ensure dynamic rendering of room types based on the data retrieved from the getTemplate query.
    - Implement cancel and save buttons for easy navigation and submission, respectively.

---
+++ src/pages/configurations/room/CreateRoom.jsx
@@ -0,0 +1,224 @@
+import { useNavigate } from "react-router-dom";
+import { enqueueSnackbar } from "notistack";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  TextField,
+  Select,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { ClearOutlined } from "@mui/icons-material";
+import * as Yup from "yup";
+import { Formik } from "formik";
+
+import {
+  useCreateRoomMutation,
+  useGetRoomTemplateQuery,
+} from "src/store/slices/configurations/roomApiSlice";
+import MainCard from "src/components/MainCard";
+
+const CreateRoom = () => {
+  const navigate = useNavigate();
+
+  const { data: getTemplate } = useGetRoomTemplateQuery();
+  const [createRoom, { isLoading }] = useCreateRoomMutation();
+
+  return (
+    <Grid item xs={12} md={7} lg={8}>
+      <Grid container alignItems="center" justifyContent="space-between">
+        <Grid item>
+          <Typography variant="h5" gutterBottom>
+            Add Room
+          </Typography>
+        </Grid>
+        <Grid item />
+      </Grid>
+
+      <MainCard sx={{ mt: 2 }} content={false}>
+        <Box sx={{ p: 2 }}>
+          <Formik
+            initialValues={{
+              name: "",
+              price: "",
+              room_type: "",
+            }}
+            validationSchema={Yup.object().shape({
+              name: Yup.string().required("Name is required"),
+              price: Yup.number().required("Price is required"),
+              room_type: Yup.string().required("Room Type is required"),
+            })}
+            onSubmit={async (values, { setStatus, setSubmitting }) => {
+              try {
+                await createRoom({
+                  name: values.name,
+                  price: values.price,
+                  room_type: values.room_type,
+                }).unwrap();
+                navigate(-1);
+                enqueueSnackbar("Room created successfully.", {
+                  variant: "success",
+                });
+
+                setStatus({ success: false });
+                setSubmitting(false);
+              } catch (err) {
+                setStatus({ success: false });
+                setSubmitting(false);
+              }
+            }}
+          >
+            {({
+              errors,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <Stack spacing={1}>
+                      <TextField
+                        fullWidth
+                        variant="outlined"
+                        name="name"
+                        value={values.name}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        label="Name"
+                        error={Boolean(touched.name && errors.name)}
+                      />
+                      {touched.name && errors.name && (
+                        <Typography variant="body2" color="error">
+                          {errors.name}
+                        </Typography>
+                      )}
+                    </Stack>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Stack spacing={1}>
+                      <TextField
+                        fullWidth
+                        variant="outlined"
+                        name="price"
+                        value={values.price}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        label="Price"
+                        type="number"
+                        inputProps={{
+                          pattern: "\\d*",
+                          min: "1",
+                        }}
+                        error={Boolean(touched.price && errors.price)}
+                      />
+                      {touched.price && errors.price && (
+                        <Typography variant="body2" color="error">
+                          {errors.price}
+                        </Typography>
+                      )}
+                    </Stack>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Stack spacing={1}>
+                      <FormControl
+                        fullWidth
+                        variant="outlined"
+                        error={Boolean(touched.room_type && errors.room_type)}
+                      >
+                        <InputLabel id="room-type-label">Room Type</InputLabel>
+                        <Select
+                          labelId="room-type-label"
+                          id="room_type"
+                          variant="outlined"
+                          name="room_type"
+                          value={values.room_type || ""}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          label="Room Type"
+                          error={Boolean(touched.room_type && errors.room_type)}
+                          endAdornment={
+                            values.room_type && (
+                              <IconButton
+                                size="small"
+                                onClick={() =>
+                                  handleChange({
+                                    target: { name: "room_type", value: "" },
+                                  })
+                                }
+                              >
+                                <ClearOutlined />
+                              </IconButton>
+                            )
+                          }
+                        >
+                          <MenuItem value="" disabled>
+                            Select a room type
+                          </MenuItem>
+                          {getTemplate?.roomTypeOptions &&
+                            Object.keys(getTemplate.roomTypeOptions).map(
+                              (key) => (
+                                <MenuItem key={key} value={key}>
+                                  {getTemplate.roomTypeOptions[key]}
+                                </MenuItem>
+                              )
+                            )}
+                        </Select>
+                      </FormControl>
+                      {touched.room_type && errors.room_type && (
+                        <Typography variant="body2" color="error">
+                          {errors.room_type}
+                        </Typography>
+                      )}
+                    </Stack>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    container
+                    justifyContent="flex-end"
+                    spacing={1}
+                  >
+                    <Grid item>
+                      <Button
+                        disableElevation
+                        fullWidth
+                        variant="outlined"
+                        onClick={() => navigate(-1)}
+                      >
+                        Cancel
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button
+                        disableElevation
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        disabled={isSubmitting || isLoading}
+                        sx={{ marginRight: 5 }}
+                      >
+                        Save
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </form>
+            )}
+          </Formik>
+        </Box>
+      </MainCard>
+    </Grid>
+  );
+};
+
+export default CreateRoom;
