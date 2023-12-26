726fdebf16263dfea3fe9394f79ac43640ead6d0
YohannesL <37918606+yohannesLilay@users.noreply.github.com>
2023-12-26 20:42:57 +0300

    refactor: Improve null-check in PermissionGuard
    Enhance the null-check for the "userInfo.permissions" property in the PermissionGuard component. Introduce the optional chaining operator to ensure a more robust check for the presence of "userInfo" and its "permissions" property. This change provides better compatibility with potentially undefined or null values.

--- src/components/PermissionGuard.jsx
+++ src/components/PermissionGuard.jsx
@@ -4,7 +4,10 @@ import PropTypes from "prop-types";
 const PermissionGuard = ({ permission, children }) => {
   const { userInfo } = useSelector((state) => state.auth);
 
-  if (!permission || (userInfo && userInfo.permissions.includes(permission))) {
+  if (
+    !permission ||
+    (userInfo && userInfo?.permissions?.includes(permission))
+  ) {
     return children;
   } else {
     return null;
